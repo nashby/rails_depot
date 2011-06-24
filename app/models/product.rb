1595b01974b9f89f569a841ed16ed1ac6884c657
Vasiliy Ermolovich <younash@gmail.com>
2011-06-24 11:58:38 +0300

    Tas C

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :length => {:minimum => 10, :message => 'OMG! Title is too short'}, :uniqueness => true
