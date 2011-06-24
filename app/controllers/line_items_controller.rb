903fd690d131753d119a2ce52bc8f21bfbbde93f
Vasiliy Ermolovich <younash@gmail.com>
2011-06-24 14:49:58 +0300

    Task E

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -43,11 +43,11 @@ class LineItemsController < ApplicationController
     @cart = current_cart
     session[:counter] = 0
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart, :notice => 'Line item was successfully created.') }
+        format.html { redirect_to(@line_item.cart) }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
